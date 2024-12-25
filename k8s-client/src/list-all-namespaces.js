d4da00bc6935b001973d7172ea448a12fad6b71c
jeldikk <jeldi.kamal2013@gmail.com>
2024-12-25 20:42:32 +0530

    add changes to store namespace data as JSON

--- k8s-client/src/list-all-namespaces.js
+++ k8s-client/src/list-all-namespaces.js
@@ -1,5 +1,8 @@
-// const k8s = require("@kubernetes/client-node");
-import * as k8s from "@kubernetes/client-node";
+const k8s = require("@kubernetes/client-node");
+const fs = require("fs/promises");
+const path = require("path");
+
+// import * as k8s from "@kubernetes/client-node";
 
 const kubeConfig = new k8s.KubeConfig();
 kubeConfig.loadFromDefault();
@@ -8,9 +11,20 @@ console.log({ defaultConfig: kubeConfig });
 const k8sApi = kubeConfig.makeApiClient(k8s.CoreV1Api);
 
 async function main() {
+  const namespaceList = [];
   try {
     const res = await k8sApi.listNamespace();
+
     console.log({ res });
+    res.items.forEach((namespace) => {
+      namespaceList.push(JSON.parse(JSON.stringify(namespace)));
+    });
+    // console.log(namespaceList);
+    await fs.writeFile(
+      path.resolve(__dirname, "./namespacelist.json"),
+      JSON.stringify(namespaceList)
+    );
+    console.log("Written data to file");
   } catch (err) {
     console.error(err);
   }
