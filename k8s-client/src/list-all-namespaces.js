841a7c861cadfde7f1d0d50e81032113fb33d4f9
jeldikk <jeldi.kamal2013@gmail.com>
2024-12-26 11:20:50 +0530

    change code compatible to es module syntax

--- k8s-client/src/list-all-namespaces.js
+++ k8s-client/src/list-all-namespaces.js
@@ -1,12 +1,18 @@
-const k8s = require("@kubernetes/client-node");
-const fs = require("fs/promises");
-const path = require("path");
+// const k8s = require("@kubernetes/client-node");
+// const fs = require("fs/promises");
+// const path = require("path");
 
-// import * as k8s from "@kubernetes/client-node";
+import * as k8s from "@kubernetes/client-node";
+import fs from "fs/promises";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const filePath = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filePath);
 
 const kubeConfig = new k8s.KubeConfig();
 kubeConfig.loadFromDefault();
-kubeConfig.setCurrentContext("minikube"); // you can set the context after loading from default
+//kubeConfig.setCurrentContext("); // you can set the context after loading from default
 
 // console.log({ defaultConfig: kubeConfig });
 console.log(
@@ -21,16 +27,13 @@ async function main() {
   try {
     const res = await k8sApi.listNamespace();
 
-    k8sApi.get;
-
-    console.log({ res });
     res.items.forEach((namespace) => {
       namespaceList.push(JSON.parse(JSON.stringify(namespace)));
     });
     // console.log(namespaceList);
     await fs.writeFile(
-      path.resolve(__dirname, "./namespacelist.json"),
-      JSON.stringify(namespaceList)
+      path.resolve(dirname, "./namespacelist.json"),
+      JSON.stringify(namespaceList, null, 2)
     );
     console.log("Written data to file");
   } catch (err) {
