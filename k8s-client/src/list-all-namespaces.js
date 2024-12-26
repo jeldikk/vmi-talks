a552853bec653d844bf9ba9ae2039395d57e2514
jeldikk <jeldi.kamal2013@gmail.com>
2024-12-26 10:52:27 +0530

    add statement to set current context

--- k8s-client/src/list-all-namespaces.js
+++ k8s-client/src/list-all-namespaces.js
@@ -6,7 +6,13 @@ const path = require("path");
 
 const kubeConfig = new k8s.KubeConfig();
 kubeConfig.loadFromDefault();
-console.log({ defaultConfig: kubeConfig });
+kubeConfig.setCurrentContext("minikube"); // you can set the context after loading from default
+
+// console.log({ defaultConfig: kubeConfig });
+console.log(
+  "The current context we are looking at :",
+  kubeConfig.getCurrentContext()
+);
 
 const k8sApi = kubeConfig.makeApiClient(k8s.CoreV1Api);
 
@@ -14,6 +20,8 @@ async function main() {
   const namespaceList = [];
   try {
     const res = await k8sApi.listNamespace();
+
+    k8sApi.get;
 
     console.log({ res });
     res.items.forEach((namespace) => {
